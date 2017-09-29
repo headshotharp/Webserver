8c5a668402ba830108c223fd93fe7f37daefd564
Simon <simon@gmasil.de>
2017-09-30 01:18:00 +0200

    added unit tests for tomcat

--- src/test/java/de/headshotharp/web/test/Tester.java
+++ src/test/java/de/headshotharp/web/test/Tester.java
@@ -1,17 +1,65 @@
 package de.headshotharp.web.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.headshotharp.web.data.UserDataProvider;
+import de.headshotharp.web.data.type.Player;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class Tester {
+	@Autowired
+	private UserDataProvider userDataProvider;
+
+	@Value("${server.port}")
+	private int port;
+
+	private String baseUrl = null;
+
+	@Before
+	public void initialize() throws MalformedURLException {
+		baseUrl = "http://localhost:" + port + "/";
+	}
+
 	@Test
-	public void test() {
-		assertThat(true).isTrue();
+	public void testConnection() throws InterruptedException, IOException {
+		assertThat(port).isGreaterThan(0);
+		assertThat(baseUrl).isNotNull();
+		Response response = Jsoup.connect(baseUrl).execute();
+		assertThat(response).isNotNull();
+		assertThat(response.statusCode()).isEqualTo(200);
+		Document doc = response.parse();
+		assertThat(doc).isNotNull();
+	}
+
+	@Test
+	public void testPlayerlist() throws IOException {
+		Document doc = Jsoup.connect(baseUrl + "playerlist").get();
+		assertThat(doc).isNotNull();
+		Element elPlayerlist = doc.getElementsByClass("playerlist").first();
+		assertThat(elPlayerlist).isNotNull();
+		Elements elements = elPlayerlist.getElementsByClass("player");
+		assertThat(elements).isNotNull();
+		List<Player> players = userDataProvider.getPlayerListActive();
+		assertThat(players).isNotNull();
+		assertThat(elements.size()).isEqualTo(players.size());
 	}
 }
